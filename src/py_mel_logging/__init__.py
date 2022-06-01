a28eb271613854105995a710874225c8f1d1e453
Mark Potter <mpotter@phoenix-int.com>
2022-05-31 21:29:43 -0400

    Cleanup: Moved and renamed __import_python_logging() to load_dotnet_python_logging().

--- src/py_mel_logging/__init__.py
+++ src/py_mel_logging/__init__.py
@@ -7,16 +7,6 @@ except ModuleNotFoundError:
 
 __version__ = importlib_metadata.version(__name__.replace(".", "-"))
 
-
-def __import_python_logging() -> None:
-    import clr  # type: ignore
-    import os
-    import sys
-
-    sys.path.append(os.path.join(os.path.dirname(__file__), "dlls/netstandard2.0"))
-    clr.AddReference(r"PythonLogging")
-
-
-__import_python_logging()
+from .load_dotnet_python_logging import load_dotnet_python_logging
 from .python_logger_provider import create_logger_provider
 from .python_logger import PythonLogger
