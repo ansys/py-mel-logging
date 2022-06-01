a5526d5e5a71644aee3bab88c4eabb11be2929c1
Mark Potter <mpotter@phoenix-int.com>
2022-05-31 21:30:23 -0400

    Added test to display DotNet framework in use.

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -1,5 +1,12 @@
 from py_mel_logging import __version__
+from System.Runtime.InteropServices import RuntimeInformation
 
 
 def test_pkg_version():
     assert __version__ == "0.1.dev0"
+
+
+def test_framework_description():
+    print()
+    fd = RuntimeInformation.FrameworkDescription
+    print(f"Framework Description: {fd}")
