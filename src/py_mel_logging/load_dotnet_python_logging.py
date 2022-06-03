87508cc999b4ac74d2d4921e6e8859f052fc5420
Mark Potter <mpotter@phoenix-int.com>
2022-06-03 13:32:15 -0400

    .

--- src/py_mel_logging/load_dotnet_python_logging.py
+++
@@ -1,8 +0,0 @@
-def load_dotnet_python_logging() -> None:
-    """Add the Dot-Net PythonLogging assembly."""
-    import os
-    import sys
-    import clr  # type: ignore
-
-    sys.path.append(os.path.join(os.path.dirname(__file__), "dlls/netstandard2.0"))
-    clr.AddReference(r"PythonLogging")
